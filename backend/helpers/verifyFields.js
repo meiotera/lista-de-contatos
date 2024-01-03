3455c64d47d34ca09d03c861bca5225ccc56dc2b
Renan <renanfc.re@gmail.com>
2024-01-03 15:42:46 -0300

    backend - Arquivo vrifyFields.js adicionado, verifyFields adicionado no UserController

---
+++ backend/helpers/verifyFields.js
@@ -0,0 +1,20 @@
+module.exports = function verifyFields({ ...arg }) {
+  const { name, email, password, confirmPassword } = arg;
+  const camposRequeridos = ["name", "email", "password", "confirmPassword"];
+  const camposVazios = camposRequeridos.filter((campo) => !arg[campo]);
+
+  if (camposVazios.length > 0) {
+    return {
+      isError: true,
+      message: `Campo obrigatório vazio: ${camposVazios.join(", ")}`,
+    };
+  }
+
+  if (password !== confirmPassword) {
+    return { isError: true, message: `Senhas não conferem` };
+  }
+
+  return null;
+}
+
+
