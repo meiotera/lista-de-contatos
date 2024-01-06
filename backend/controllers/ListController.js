e8b90f43eb0fbfcbec510a79af99563a45f721d1
Renan Ferreira <renanfc.re@gmail.com>
2024-01-06 15:55:11 -0300

    backend - Função myContacts criada e adicionada rota minha-lista, função get para carregar os contatos cadastrados pelo usuário.

--- backend/controllers/ListController.js
+++ backend/controllers/ListController.js
@@ -1,4 +1,5 @@
-const ListModel = require("../models/ListModel");
+const List = require("../models/ListModel");
+const User = require("../models/UserModel");
 const verifyTokenUser = require("../helpers/verifyTokenUser");
 
 module.exports = class ListController {
@@ -14,7 +15,7 @@ module.exports = class ListController {
         phone,
         UserId: decodedToken.userId,
       };
-      await ListModel.create(contact);
+      await List.create(contact);
 
       res
         .status(200)
@@ -24,6 +25,31 @@ module.exports = class ListController {
         isError: true,
         message: "Erro durante o registro, tente novamente mais tarde.",
       });
+    }
+  }
+
+  static async myContacts(req, res) {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const decodedToken = await verifyTokenUser(token);
+      const userId = decodedToken.userId;
+
+      const contacts = await User.findByPk(userId, {
+        include: List,
+        order: [["created_at"]],
+      });
+
+      const contatosFiltrados = { ...contacts.get() };
+      delete contatosFiltrados.password;
+
+      res.status(200).json({
+        contatosFiltrados,
+      });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ isError: true, message: "Erro ao obter contatos do usuário" });
     }
   }
 };
--- backend/routes/ListRoutes.js
+++ backend/routes/ListRoutes.js
@@ -4,6 +4,6 @@ const ListController = require("../controllers/ListController");
 const verifyTokenUser = require("../helpers/verifyTokenUser");
 
 router.post("/criarcontato", verifyTokenUser, ListController.createContact);
-// router.post("/edit/:id", verifyTokenUser, ListController.editContact);
+router.get("/minha-lista", verifyTokenUser, ListController.myContacts);
 
 module.exports = router;
