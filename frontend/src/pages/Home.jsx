f9613be793d75bf8b988d0e323a508fd9ed3d57c
Renan Ferreira <renanfc.re@gmail.com>
2024-01-07 15:27:09 -0300

    Recorreção de escrita na home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (    
       <div>
-        <h1>Seus contato aqui!</h1>
+        <h1>Seus contatos aqui!</h1>
         <h2>Faça seu cadastro</h2>
       </div>
     
