6b781f9a97ab999f541a653f1813caa9679945aa
Renan Ferreira <renanfc.re@gmail.com>
2024-01-18 08:13:15 -0300

    backend -> função de verificação de senha modificada para verificar se campo de senha está preenchido antes de fazer a verificação de hash

--- backend/helpers/verifyFields.js
+++ backend/helpers/verifyFields.js
@@ -3,9 +3,28 @@ module.exports = function verifyFields({ ...arg }) {
   const camposRequeridos = ["name", "email", "password", "confirmPassword"];
   const camposVazios = camposRequeridos.filter((campo) => !arg[campo]);
 
+  let campoLabel = "";
+
+  switch (true) {
+    case camposVazios.includes("name"):
+      campoLabel = "Nome";
+      break;
+    case camposVazios.includes("email"):
+      campoLabel = "E-mail";
+      break;
+    case camposVazios.includes("password"):
+      campoLabel = "Senha";
+      break;
+    case camposVazios.includes("confirmPassword"):
+      campoLabel = "Confirme sua senha";
+      break;
+    default:
+      break;
+  }
+
   if (camposVazios.length > 0) {
     return {
-      message: `Campo obrigatório vazio: ${camposVazios.join(", ")}`,
+      message: `Campo obrigatório vazio: ${campoLabel}`,
     };
   }
 
--- backend/helpers/verifyPassword.js
+++ backend/helpers/verifyPassword.js
@@ -2,7 +2,13 @@ const User = require("../models/UserModel");
 const bcrypt = require("bcrypt");
 
 module.exports = async function verifyPassword(email, password) {
+  if (!password) {
+    return {
+      message: "Digite sua senha!",
+    };
+  }
   const user = await User.findOne({ where: { email: email } });
+
   try {
     const checkPassword = bcrypt.compareSync(password, user.password);
 
